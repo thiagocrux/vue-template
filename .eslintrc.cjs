62bb6dfed1b0658a584e2fffec91f0ea6721f36a
Thiago Cruz <thiagocruz.eu@gmail.com>
2024-03-01 01:08:36 -0300

    chore: config eslint and prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-recommended', 'prettier'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    // sourceType: 'module',
+  },
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'vue/no-v-html': 'off',
+    'vue/multi-word-component-names': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        singleQuote: true,
+        tabWidth: 2,
+        semi: false,
+        printWidth: 80,
+        arrowParens: 'always',
+        vueIndentScriptAndStyle: true,
+      },
+    ],
+  },
+}
